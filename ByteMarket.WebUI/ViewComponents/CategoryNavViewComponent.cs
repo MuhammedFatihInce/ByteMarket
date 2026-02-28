d0bca2a670c286d4e8a064ff188ddb52741d5271
MuhammedFatihInce <myhammedfatih@gmail.com>
2026-02-28 15:04:55 +0300

    ui(webui): kategori navigasyonu Header'dan Home/Index sayfasına taşındı

---
+++ ByteMarket.WebUI/ViewComponents/CategoryNavViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using ByteMarket.WebUI.Models.Category;
+using ByteMarket.WebUI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ByteMarket.WebUI.ViewComponents
+{
+	public class CategoryNavViewComponent : ViewComponent
+	{
+		private readonly ICategoryService _categoryService;
+
+		public CategoryNavViewComponent(ICategoryService categoryService)
+		{
+			_categoryService = categoryService;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var categories = new List<ListCategoryViewModel>();
+
+			var result = await _categoryService.GetCategoriesAsync();
+
+			if (result.Success && result.Data != null)
+			{
+				return View(result.Data);
+			}
+
+			return View(categories);
+		}
+	}
+}
